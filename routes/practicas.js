5bef8141b9cd18004a250e9c88c0e46556dc1842
Ángel Cadenas Fijo <angel.cadenas@Aytos.local>
2023-05-09 14:39:08 +0200

    Añadiendo Update de prácticas

--- routes/practicas.js
+++ routes/practicas.js
@@ -8,17 +8,17 @@ const supabase = require('../config');
 /* Mostrar prácticas según la autoescuela */
 
 router.get('/practica/:id_profesor', async (req, res) => {
-    const { id_profesor } = req.params;
-    const { data: practicas, error } = await supabase
-        .from('practicas')
-        .select('*')
-        .eq('id_profesor', id_profesor);
+  const { id_profesor } = req.params;
+  const { data: practicas, error } = await supabase
+    .from('practicas')
+    .select('*')
+    .eq('id_profesor', id_profesor);
 
-    if (error) {
-        res.status(500).json({ error: error.message });
-    } else {
-        res.status(200).json(practicas);
-    }
+  if (error) {
+    res.status(500).json({ error: error.message });
+  } else {
+    res.status(200).json(practicas);
+  }
 });
 
 
@@ -26,46 +26,76 @@ router.get('/practica/:id_profesor', async (req, res) => {
 /* Crear practica con id_alumno en null */
 
 router.post('/practica/:id_profesor', async (req, res) => {
-    const { id_profesor } = req.params;
-    const { fecha, hora, tipo } = req.body;
+  const { id_profesor } = req.params;
+  const { fecha, hora, tipo } = req.body;
 
-    const { data, error } = await supabase
-        .from('practicas')
-        .insert({ id_profesor, id_alumno: null, fecha, hora, tipo });
+  const { data, error } = await supabase
+    .from('practicas')
+    .insert({ id_profesor, id_alumno: null, fecha, hora, tipo });
+
+  if (error) {
+    res.status(500).json({ error: error.message });
+  } else {
+    res.status(201).json({ data });
+  }
+});
+
+
+
+
+/* Actualizar practica */
+
+router.put('/practica/:id_profesor/:fecha/:hora', async (req, res) => {
+  const { id_profesor, fecha, hora } = req.params;
+  const { tipo, nuevaFecha, nuevaHora } = req.body;
+
+  try {
+    const { error } = await supabase
+      .from('practicas')
+      .update({ tipo, fecha: nuevaFecha, hora: nuevaHora })
+      .eq('id_profesor', id_profesor)
+      .eq('fecha', fecha)
+      .eq('hora', hora);
 
     if (error) {
-        res.status(500).json({ error: error.message });
-    } else {
-        res.status(201).json({ data });
+      console.error(error);
+      return res.status(500).json({ error: 'Error al actualizar la práctica' });
     }
+
+    return res.status(200).json({ message: 'Práctica actualizada correctamente' });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ error: 'Error al actualizar la práctica' });
+  }
 });
+
 
 
 /* Borrar practica por su id_profesor en las practicas */
 
 router.delete('/practica/:id_profesor/:fecha/:hora', async (req, res) => {
-    const { id_profesor, fecha, hora } = req.params;
-  
-    try {
-      const { error } = await supabase
-        .from('practicas')
-        .delete()
-        .eq('id_profesor', id_profesor)
-        .eq('fecha', fecha)
-        .eq('hora', hora);
-  
-      if (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Error al borrar la práctica' });
-      }
-  
-      return res.status(200).json({ message: 'Práctica eliminada correctamente' });
-    } catch (err) {
-      console.error(err.message);
+  const { id_profesor, fecha, hora } = req.params;
+
+  try {
+    const { error } = await supabase
+      .from('practicas')
+      .delete()
+      .eq('id_profesor', id_profesor)
+      .eq('fecha', fecha)
+      .eq('hora', hora);
+
+    if (error) {
+      console.error(error);
       return res.status(500).json({ error: 'Error al borrar la práctica' });
     }
-  });
-  
+
+    return res.status(200).json({ message: 'Práctica eliminada correctamente' });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ error: 'Error al borrar la práctica' });
+  }
+});
+
 
 
 
